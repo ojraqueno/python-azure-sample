f0fc22256e76a84eff4c6591128d2a2500d99ed7
OJ <oj.raqueno@gmail.com>
2021-09-16 16:52:57 +0800

    add sqlalchemy

--- LocalFunctionProj/HttpExample/__init__.py
+++ LocalFunctionProj/HttpExample/__init__.py
@@ -2,9 +2,36 @@ import logging
 
 import azure.functions as func
 
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine('postgresql://sa@afdbserver01:Password!123@afdbserver01.postgres.database.azure.com:5432/postgres')
+
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(50))
+
+Base.metadata.create_all(engine)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    session = Session()
+    user = User(name='parker')
+    session.add(user)
+
+    session.commit()
+
+    theUser = session.query(User).filter_by(name='parker').first()
+
+    logging.info(theUser)
 
     name = req.params.get('name')
     if not name:
